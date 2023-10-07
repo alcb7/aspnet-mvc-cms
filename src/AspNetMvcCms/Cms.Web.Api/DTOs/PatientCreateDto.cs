d0d35778ff05aebf094a535e2ec324450d381b43
mehmet134 <mehmetkrs42@gmail.com>
2023-10-07 20:25:18 +0300

    .

--- src/AspNetMvcCms/Cms.Web.Api/DTOs/PatientCreateDto.cs
+++ src/AspNetMvcCms/Cms.Web.Api/DTOs/PatientCreateDto.cs
@@ -13,7 +13,8 @@ namespace Cms.Web.Api.DTOs
         [Required(ErrorMessage = "Soyisim zorunludur.")]
         [StringLength(255, ErrorMessage = "Soyisim en fazla 255 karakter olmalıdır.")]
         public string Surname { get; set; } = string.Empty;
+		[Required]
+		public int CategoryId { get; set; }
 
-        
-    }
+	}
 }
