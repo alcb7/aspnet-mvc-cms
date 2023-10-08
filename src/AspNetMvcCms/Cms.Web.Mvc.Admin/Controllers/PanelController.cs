1bb2408f054dbed38692caffaf91fc45c373905c
mehmet134 <mehmetkrs42@gmail.com>
2023-10-08 15:15:40 +0300

    .

---
+++ src/AspNetMvcCms/Cms.Web.Mvc.Admin/Controllers/PanelController.cs
@@ -0,0 +1,30 @@
+﻿using Cms.Data.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Web.Mvc.Admin.Controllers
+{
+	public class PanelController : Controller
+	{
+		private readonly HttpClient _httpClient;
+
+		private readonly string _apiDoctor = "https://localhost:7188/api/Doctors";
+		private readonly string _apiPatient = "https://localhost:7188/api/Patients";
+		public PanelController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+		public async Task<ActionResult> Doctors()
+		{
+			var model = await _httpClient.GetFromJsonAsync<List<DoctorEntity>>(_apiDoctor);
+
+			return View(model);
+		}
+
+		public async Task<ActionResult> Patients()
+		{
+			var model = await _httpClient.GetFromJsonAsync<List<PatientEntity>>(_apiPatient);
+
+			return View(model);
+		}
+	}
+}
