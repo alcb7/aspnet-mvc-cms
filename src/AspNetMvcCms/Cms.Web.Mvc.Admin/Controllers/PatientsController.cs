6d1c1ebf38c5dbc9b0add1170f881d3eaa96d1a7
alcb7 <canbulan1849@gmail.com>
2023-10-11 00:26:20 +0300

    admin patient kısmı halledildi

---
+++ src/AspNetMvcCms/Cms.Web.Mvc.Admin/Controllers/PatientsController.cs
@@ -0,0 +1,25 @@
+﻿using Cms.Data.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Web.Mvc.Admin.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly string _apiPatient = "https://localhost:7188/api/Patients";
+
+        public PatientsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ActionResult> GetPatients()
+        {
+            var model = await _httpClient.GetFromJsonAsync<List<PatientEntity>>(_apiPatient);
+
+            return View(model);
+        }
+
+
+    }
+}
