ab1cf8dc26a33efb721c354b658529f1b60cf32e
alcb7 <canbulan1849@gmail.com>
2023-10-11 21:40:15 +0300

    sonhal

--- src/AspNetMvcCms/Cms.Web.Mvc.Admin/Controllers/PatientsController.cs
+++ src/AspNetMvcCms/Cms.Web.Mvc.Admin/Controllers/PatientsController.cs
@@ -1,5 +1,7 @@
 ﻿using Cms.Data.Models.Entities;
+using Cms.Web.Mvc.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace Cms.Web.Mvc.Admin.Controllers
 {
@@ -19,7 +21,57 @@ namespace Cms.Web.Mvc.Admin.Controllers
 
             return View(model);
         }
+		[HttpGet]
+		public async Task<ActionResult> UpdatePatients(int id)
+		{
+			// İlgili blogun bilgilerini almak için id kullanın
+			var blog = await _httpClient.GetFromJsonAsync<PatientEntity>($"{_apiPatient}/{id}");
+			if (blog == null)
+			{
+				return NotFound(); // Blog bulunamadıysa 404 hatası döndürün veya başka bir işlem yapın.
+			}
+
+			// Blog bilgilerini bir DTO'ya aktarabilirsiniz
+			var patientDto = new PatientViewModel
+			{
+				Id = id,
+				Name = blog.Name,
+				Surname = blog.Surname,
+				Email = blog.Email,
+
+			};
+
+			return View(patientDto);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> UpdatePatients(int id, PatientViewModel dto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(dto);
+			}
+			var patientEntity = new PatientEntity
+			{
+				Id = id,
+				Name = dto.Name,
+				Surname = dto.Surname,
+				Email = dto.Email,
+
+			};
+
+			
+			// Güncelleme işlemi için HTTP PUT veya PATCH isteği gönderin
+			var response = await _httpClient.PutAsJsonAsync($"{_apiPatient}/{id}", patientEntity);
+
+			if (response.IsSuccessStatusCode)
+			{
+				ViewBag.Message = "Blog Başarıyla güncellendi.";
+			}
+
+			return View(dto);
+		}
 
 
-    }
+	}
 }
---
+++ src/AspNetMvcCms/Cms.Web.Mvc.Admin/Models/PatientViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cms.Web.Mvc.Admin.Models
+{
+	public class PatientViewModel
+	{
+		public int Id { get; set; }
+
+	
+		public string Name { get; set; } = string.Empty;
+
+		public string Surname { get; set; } = string.Empty;
+
+		
+
+		public string Email { get; set; } = string.Empty;
+	}
+}
