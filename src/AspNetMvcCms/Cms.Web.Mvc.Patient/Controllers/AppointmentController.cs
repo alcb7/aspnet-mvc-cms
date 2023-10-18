d0261ba15f7bc1322aca0e7738692e505e4c2c52
alcb7 <canbulan1849@gmail.com>
2023-10-18 15:18:21 +0300

    sonnn

--- src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/AppointmentController.cs
+++ src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/AppointmentController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cms.Data.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cms.Web.Mvc.Patient.Controllers
 {
     public class AppointmentController : Controller
     {
-        public IActionResult Index()
+
+        private readonly HttpClient _httpClient;
+
+
+        private readonly string _apiDcategories = "https://localhost:7188/api/DoctorCategories/";
+        public AppointmentController(HttpClient httpClient)
         {
-            return View();
+            _httpClient = httpClient;
+        }
+        
+        public async Task<ActionResult> Index()
+        {
+            var model = await _httpClient.GetFromJsonAsync<List<DoctorCategoryEntity>>(_apiDcategories);
+
+            return View(model);
         }
     }
 }
