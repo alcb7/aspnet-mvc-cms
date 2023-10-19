27d0b2c82d8c6ed6b9d2817b832880c9aa7a7090
mehmet134 <mehmetkrs42@gmail.com>
2023-10-19 15:27:32 +0300

    .

--- src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/BlogController.cs
+++ src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/BlogController.cs
@@ -1,16 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cms.Data.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cms.Web.Mvc.Patient.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        private readonly string _apiBlog = "https://localhost:7188/api/Blogs/";
+        public BlogController(HttpClient httpClient)
         {
-            return View();
+            _httpClient = httpClient;
         }
-        public IActionResult Detail()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var model = await _httpClient.GetFromJsonAsync<List<BlogEntity>>(_apiBlog);
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> Detail(int id)
+        {
+            // Belirli bir doktorun detaylarını getir.
+            var doctor = await _httpClient.GetFromJsonAsync<BlogEntity>(_apiBlog + id);
+
+            if (doctor == null)
+            {
+                // Belirli bir doktor bulunamazsa hata sayfasına yönlendirme yapılabilir.
+                return NotFound();
+            }
+
+            return View(doctor);
         }
     }
 }
