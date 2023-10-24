43176750cd3aff977f67ec96e47fa2bb05ee0a6e
mehmet134 <mehmetkrs42@gmail.com>
2023-10-24 17:41:28 +0300

    .

--- src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/AppointmentController.cs
+++ src/AspNetMvcCms/Cms.Web.Mvc.Patient/Controllers/AppointmentController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cms.Data.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http;
 
 namespace Cms.Web.Mvc.Patient.Controllers
 {
     public class AppointmentController : Controller
     {
+        private readonly HttpClient _httpClient;
+
+        private readonly string _apiDoctor = "https://localhost:7188/api/Doctors/";
+        public AppointmentController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        //[HttpGet]
+        //public async Task<JsonResult> LoadDoctor(int categoryId)
+        //{
+        //    var doctorlist = await _httpClient.GetFromJsonAsync<List<DoctorEntity>>(_apiDoctor);
+
+        //    var newDoctors = doctorlist?.Where(d => d.CategoryId == categoryId).ToList();
+
+
+        //    return Json(new SelectList(newDoctors, "Id", "Name"));
+        //}
+        [HttpGet]
+        public async Task<JsonResult> LoadDoctor(int categoryId)
+        {
+            var doctorlist = await _httpClient.GetFromJsonAsync<List<DoctorEntity>>(_apiDoctor);
+
+            var newDoctors = doctorlist?
+                .Where(d => d.CategoryId == categoryId)
+                .Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name // veya d.FullName veya başka bir özellik
+                })
+                .ToList();
+
+            return Json(newDoctors);
+        }
+
     }
+
 }
---
+++ src/AspNetMvcCms/Cms.Web.Mvc.Patient/Models/AppointmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cms.Web.Mvc.Patient.Models
+{
+	public class AppointmentViewModel
+	{
+		public int CategoryId{ get; set; }
+
+		public int DoctorId { get; set;}
+
+		public DateTime? AppointmentDate { get; set; }
+
+
+	}
+}
